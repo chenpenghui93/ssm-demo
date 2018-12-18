3269253a7263ddeca3c8bfe8e7edf8e723f41044
chenpenghui93 <chenpenghui93@hotmail.com>
2018-12-18 21:12:24 +0800

    2018年12月18日 更新启动类

--- src/main/java/com/example/ssmdemo/Application.java
+++ src/main/java/com/example/ssmdemo/Application.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * 启动类
+ *
+ * @author cph
+ * @version 1.0
+ * @date 2018/12/17
+ */
 @SpringBootApplication
 @EnableTransactionManagement
-public class SsmDemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SsmDemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
