331a1170e33124df037e613ba2bf4b1aa6bc3139
chenpenghui <chenpenghui@hisense.com>
2018-12-24 18:01:43 +0800

    2018年12月24日 java mail

--- src/main/java/com/example/ssmdemo/helloworld/SendEmail.java
+++ src/main/java/com/example/ssmdemo/helloworld/SendEmail.java
@@ -1,8 +1,13 @@
 package com.example.ssmdemo.helloworld;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 /**
@@ -18,55 +23,68 @@ public class SendEmail {
 
     public static void main(String[] args) {
 
-        sendSimpleEmail();
+        sendEmail();
 
     }
 
-    public static void sendSimpleEmail(){
+    public static void sendEmail() {
+        //收件邮箱
+        String to = "cph_2001@163.com";
+        //发件邮箱
+        String from = "mcmadmin@163.com";
 
-        String to = "";
-        String from = "";
-        String host = "smtp.163.com";
-        String subject = "This is the Subject Line!";
-        String content = "This is actual message";
-
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.smtp.host", host);
+        Properties properties = new Properties();
+        //指定发送邮件的主机
+        properties.put("mail.host", "smtp.163.com");
+        //指定邮件发送协议
+        properties.put("mail.transport.protocol", "smtp");
         properties.put("mail.smtp.auth", true);
 
-        Session session = Session.getDefaultInstance(properties, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("", "");
-            }
-        });
-
+        //获取session对象
+        Session session = Session.getInstance(properties);
+        //控制台打印会话日志
         session.setDebug(true);
 
         try {
+            //通过session获取Transport对象
+            Transport transport = session.getTransport();
+            transport.connect(from, "客户端授权密码");
 
+            //创建邮件对象
             MimeMessage message = new MimeMessage(session);
+            //设置发件人
             message.setFrom(new InternetAddress(from));
-            //发送-TO，抄送-CC，密送-BCC
+            //设置收件人，type：TO/CC/BCC分别表示发送/抄送/密送
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
-            message.setSubject(subject);
-            message.setText(content);
-            Transport.send(message);
-            System.out.println("sent message successfully···");
+            //邮件主题
+            message.setSubject("神秘的西夏");
 
-        }catch (Exception e) {
+            //邮件正文-简单邮件
+            //message.setText("请查收数据并反馈意见");
 
+            //邮件正文-HTML邮件
+            //message.setContent("<h1>hello, world!<h1>", "text/html");
+
+            //邮件正文-带附件邮件
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText("大白高的王，有十种吉祥？");
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+            //附件
+            messageBodyPart = new MimeBodyPart();
+            String filename = "D:\\devRepo\\ssm-demo\\src\\main\\resources\\static\\img\\road.jpg";
+            DataSource dataSource = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(dataSource));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+            message.setContent(multipart);
+
+            //发送邮件
+            transport.sendMessage(message, message.getAllRecipients());
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-
-    }
-
-    public static void sendHTMLEmail(){
-
-    }
-
-    public static void sendAttachmentsEmail(){
-
     }
 
 }
