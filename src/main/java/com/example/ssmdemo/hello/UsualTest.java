baf4d8104262074b0015afbf5b3a661c993eb822
chenpenghui <chenpenghui@hisense.com>
2019-01-10 09:30:20 +0800

    test

--- src/main/java/com/example/ssmdemo/hello/UsualTest.java
+++ src/main/java/com/example/ssmdemo/hello/UsualTest.java
@@ -7,13 +7,41 @@ package com.example.ssmdemo.hello;
  */
 public class UsualTest {
     public static void main(String[] args) {
-        mathMethodTest();
+        //mathMethodTest();
+        //startWithMethod();
+        splitMethod();
     }
 
     public static void mathMethodTest() {
         final int COUNT = 20;
         for (int i = 0; i < COUNT; i++) {
             System.out.println(Math.random());
+        }
+    }
+
+    public static void startWithMethod() {
+        String string = "www.runoob.com";
+
+        //true
+        System.out.println(string.startsWith("www"));
+        //false
+        System.out.println(string.startsWith("runoob"));
+        //true
+        System.out.println(string.startsWith("runoob", 4));
+    }
+
+    static void splitMethod() {
+        String s1 = "天地玄黄，宇宙洪荒";
+        String s2 = "a|b|c|d";
+
+        for (String st1 : s1.split("，")) {
+            System.out.println(st1);
+        }
+        for (String st2 : s2.split("\\|")) {
+            System.out.println(st2);
+        }
+        for (String st2 : s2.split("\\|", 3)) {
+            System.out.println(st2);
         }
     }
 }
