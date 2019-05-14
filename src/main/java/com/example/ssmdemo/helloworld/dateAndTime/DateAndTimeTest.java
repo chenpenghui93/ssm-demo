696b1304b97db6511fee293f8de72a413a0ec409
chenpenghui <chenpenghui@hisense.com>
2019-05-14 17:40:38 +0800

    test

--- src/main/java/com/example/ssmdemo/helloworld/dateAndTime/DateAndTimeTest.java
+++ src/main/java/com/example/ssmdemo/helloworld/dateAndTime/DateAndTimeTest.java
@@ -10,9 +10,11 @@ import java.util.Date;
 public class DateAndTimeTest {
 
     public static void main(String[] args) {
+        //获取指定格式的当前日期
         Date date = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
         System.out.println(sdf.format(date));
 
+        //时间
     }
 }
