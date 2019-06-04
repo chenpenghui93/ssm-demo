38438633c7acbf12521dbf31a47d8b3033bf4cb5
chenpenghui <chenpenghui@hisense.com>
2019-06-04 11:16:18 +0800

    JsonUtils

--- src/main/java/com/example/ssmdemo/controller/UserController.java
+++ src/main/java/com/example/ssmdemo/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.example.ssmdemo.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.example.ssmdemo.dao.UserDao;
 import com.example.ssmdemo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
