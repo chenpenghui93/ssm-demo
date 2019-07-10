a5369823a12051c30db3f308f27811bbf71c6eb3
chenpenghui93 <chenpenghui93@hotmail.com>
2019-07-10 22:15:57 +0800

    日期时间问题

--- src/main/java/com/example/ssmdemo/util/datetime/DateTimeUtils.java
+++ src/main/java/com/example/ssmdemo/util/datetime/DateTimeUtils.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * @author cph
@@ -12,7 +13,69 @@ import java.util.Date;
  */
 public class DateTimeUtils {
     public static void main(String[] args) {
+//        string2LocalDate();
+//        localDateTest();
+//        localDate2Date();
+        unixTimeTest();
 
+    }
+
+    private static void unixTimeTest() {
+
+        long t = System.currentTimeMillis();
+        System.out.println("long time " + t);
+
+        // 当前时区
+        SimpleDateFormat sdf_default = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("current " + sdf_default.format(t));
+
+        // +8:00
+        SimpleDateFormat sdf_8 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdf_8.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+        System.out.println("GMT+8:00 " + sdf_8.format(t));
+
+        // +7:00
+        SimpleDateFormat sdf_7 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdf_7.setTimeZone(TimeZone.getTimeZone("GMT+7:00"));
+        System.out.println("GMT+7:00 " + sdf_7.format(t));
+
+        // -9:00
+        SimpleDateFormat sdf_la = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdf_la.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+        System.out.println("America/Los_Angeles " + sdf_la.format(t));
+
+    }
+
+    private static void localDateTest() {
+        System.out.println("当天：" + LocalDate.now());
+        System.out.println("是否在当天之前：" + LocalDate.now().minusDays(1).isBefore(LocalDate.now()));
+        System.out.println("是否在当天之后：" + LocalDate.now().plusDays(1).isAfter(LocalDate.now()));
+        System.out.println("是否在当天：" + LocalDate.now().isEqual(LocalDate.now()));
+        System.out.println("今年是否是闰年：" + LocalDate.now().isLeapYear());
+
+        LocalDate date = LocalDate.of(2019, 5, 30);
+        if (LocalDate.now().plusDays(1).toString().equals(date.toString())) {
+            System.out.println(11111);
+            System.out.println(LocalDate.now().plusDays(1));
+        } else if (LocalDate.now().plusDays(2).toString().equals(date.toString())) {
+            System.out.println(2222222);
+            System.out.println(LocalDate.now().plusDays(2));
+        }
+    }
+
+    private static void localDate2Date() {
+        ZoneId zoneId = ZoneId.systemDefault();
+        LocalDate localDate = LocalDate.now();
+        ZonedDateTime zdt = localDate.atStartOfDay(zoneId);
+
+        Date date = Date.from(zdt.toInstant());
+
+        System.out.println(localDate);
+        System.out.println(zdt);
+        System.out.println(date);
+    }
+
+    private static void string2LocalDate() {
         //Java 8 – How to convert String to LocalDate
         //https://www.mkyong.com/java8/java-8-how-to-convert-string-to-localdate/
 
@@ -76,7 +139,7 @@ public class DateTimeUtils {
             e.printStackTrace();
         }
 
-        System.out.println("LocalDate.now(): "+ LocalDate.now());
+        System.out.println("LocalDate.now(): " + LocalDate.now());
     }
 
 }
