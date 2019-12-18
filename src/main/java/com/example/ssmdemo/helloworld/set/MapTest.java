ed29c680838da8ca7b86c97ed940bf7d7a246711
chenpenghui93 <chenpenghui93@hotmail.com>
2019-12-18 14:03:03 +0800

    test

---
+++ src/main/java/com/example/ssmdemo/helloworld/set/MapTest.java
@@ -0,0 +1,37 @@
+package com.example.ssmdemo.helloworld.set;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author cph
+ * @date 2019/12/11
+ */
+public class MapTest {
+
+    public static void main(String[] args) {
+        Map<String, Employee> staff = new HashMap<>();
+        staff.put("zhangsan", new Employee(1L, "zhangsan", "111111"));
+        staff.put("lisi", new Employee(2L, "lisi", "22222"));
+        staff.put("wangwu", new Employee(3L, "wangwu", "333333"));
+
+        System.out.println(staff);
+
+        staff.remove("zhangsan");
+        System.out.println(staff);
+
+        staff.put("liuliu", new Employee(4L, "liuliu", "44444"));
+
+        System.out.println(staff.get("wangwu"));
+
+        for (Map.Entry<String, Employee> entry : staff.entrySet()) {
+            String key = entry.getKey();
+            Employee value = entry.getValue();
+            System.out.println("key: " + key + ", value: " + value);
+        }
+
+        System.out.println(staff.keySet());
+        System.out.println(staff.values());
+
+    }
+}
