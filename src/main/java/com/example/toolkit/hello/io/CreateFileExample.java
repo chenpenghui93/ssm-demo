bbe9bb2ffe22242b58f590de575b5efdac2d1484
chenpenghui <chenpenghui@hisense.com>
2019-12-22 15:39:45 +0800

    java io

---
+++ src/main/java/com/example/toolkit/hello/io/CreateFileExample.java
@@ -0,0 +1,22 @@
+package com.example.toolkit.hello.io;
+
+import java.io.File;
+
+/**
+ * @author cph
+ */
+public class CreateFileExample {
+    public static void main(String[] args) {
+        try {
+            File file = new File("D:\\newFile.txt");
+            if (file.createNewFile()) {
+                System.out.println("file created.");
+            } else {
+                System.out.println("file already exists.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
