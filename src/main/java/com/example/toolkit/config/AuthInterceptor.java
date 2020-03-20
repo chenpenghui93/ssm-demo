767aebd470a19ae25f14298c40a3ed6986637679
chenpenghui93 <chenpenghui93@hotmail.com>
2020-03-20 10:45:07 +0800

    test Filter && Interceptor

--- src/main/java/com/example/toolkit/config/AuthInterceptor.java
+++ src/main/java/com/example/toolkit/config/AuthInterceptor.java
@@ -50,7 +50,6 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         System.out.println("AuthInterceptor.postHandle()=================");
-
     }
 
     /**
@@ -65,6 +64,5 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         System.out.println("AuthInterceptor.afterCompletion()=================");
-
     }
 }
