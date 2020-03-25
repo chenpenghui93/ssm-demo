a89e024b2c75fb45f01ff7f1311a4d0292e732be
chenpenghui93 <chenpenghui93@hotmail.com>
2020-03-26 00:28:36 +0800

    test starter

---
+++ src/main/java/com/example/toolkit/demo/Test.java
@@ -0,0 +1,15 @@
+package com.example.toolkit.demo;
+
+
+import com.example.HelloWorldService;
+
+/**
+ * @Author cph
+ * @Date 2020/3/25
+ */
+public class Test {
+    public static void main(String[] args) {
+        HelloWorldService service = new HelloWorldService();
+        service.hello();
+    }
+}
