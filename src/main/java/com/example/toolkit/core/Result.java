bf4a49d01db4e6e1f35df497f28bccb01ed1e43b
chenpenghui <chenpenghui@hisense.com>
2020-07-11 16:18:32 +0800

    优化

--- src/main/java/com/example/toolkit/core/Result.java
+++ src/main/java/com/example/toolkit/core/Result.java
@@ -18,28 +18,58 @@ public class Result extends HashMap<String, Object> {
         put("msg", "success");
     }
 
+    /**
+     * 返回默认成功消息
+     *
+     * @return
+     */
     public static Result ok() {
         return new Result();
     }
 
+    /**
+     * 返回自定义Map
+     *
+     * @param map
+     * @return
+     */
     public static Result ok(Map<String, Object> map) {
         Result result = new Result();
         result.putAll(map);
         return result;
     }
 
+    /**
+     * 返回自定义Object
+     *
+     * @param obj
+     * @return
+     */
     public static Result ok(Object obj) {
         Result result = new Result();
         result.put("data", obj);
         return result;
     }
 
+    /**
+     * 返回自定义消息
+     *
+     * @param msg
+     * @return
+     */
     public static Result ok(String msg) {
         Result result = new Result();
         result.put("msg", msg);
         return result;
     }
 
+    /**
+     * 返回自定义编码、消息
+     *
+     * @param code
+     * @param msg
+     * @return
+     */
     public static Result error(int code, String msg) {
         Result result = new Result();
         result.put("code", code);
@@ -47,10 +77,21 @@ public class Result extends HashMap<String, Object> {
         return result;
     }
 
+    /**
+     * 返回500、自定义消息
+     *
+     * @param msg
+     * @return
+     */
     public static Result error(String msg) {
         return error(500, msg);
     }
 
+    /**
+     * 返回默认错误消息
+     *
+     * @return
+     */
     public static Result error() {
         return error(500, "未知异常，请联系管理员");
     }
--- src/main/java/com/example/toolkit/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.toolkit.service;
-
-/**
- * @Author cph
- * @Date 2020/3/20
- */
-public class UserService {
-}
