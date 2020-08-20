455bd9b61ee140f843c76f4378c77216bc157652
chenpenghui <chenpenghui@hisense.com>
2020-08-20 18:51:26 +0800

    转换工具类

--- src/main/java/com/example/toolkit/common/ConvertUtil.java
+++ src/main/java/com/example/toolkit/common/ConvertUtil.java
@@ -14,11 +14,11 @@ public class ConvertUtil {
     /**
      * Java 对象转换为 JSON 字符串
      *
-     * @param obj
+     * @param object
      * @return String
      */
-    public static final String toJson(final Object obj) {
-        return JSON.toJSONString(obj);
+    public static final String toJson(final Object object) {
+        return JSON.toJSONString(object);
     }
 
     /**
@@ -36,10 +36,61 @@ public class ConvertUtil {
     /**
      * 小驼峰转下划线,例 "testData" -> "test_data"
      *
-     * @param str
+     * @param string
      * @return
      */
-    public static String lowerCamel2Underscope(String str) {
-        return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, str);
+    public static String lowerCamel2Underscope(String string) {
+        return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, string);
     }
+
+    /**
+     * 大驼峰转下划线，例 "TestData" -> "test_data"
+     *
+     * @param string
+     * @return
+     */
+    public static String upperCamel2Underscope(String string) {
+        return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, string);
+    }
+
+    /**
+     * 小驼峰转连字符，例 "testData" -> "test-data"
+     *
+     * @param string
+     * @return
+     */
+    public static String lowerCamel2Hyphen(String string) {
+        return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_HYPHEN, string);
+    }
+
+    /**
+     * 下划线转小驼峰 例 "test_data" -> "testData"
+     *
+     * @param string
+     * @return
+     */
+    public static String underscope2LowerCamel(String string) {
+        return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, string);
+    }
+
+    /**
+     * 下划线转大驼峰 例 "test_data" -> "TestData"
+     *
+     * @param string
+     * @return
+     */
+    public static String underscope2UpperCamel(String string) {
+        return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, string);
+    }
+
+    /**
+     * 连字符转小驼峰 例 "test-data" -> "testData"
+     *
+     * @param string
+     * @return
+     */
+    public static String hyphen2LowerCamel(String string) {
+        return CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, string);
+    }
+
 }
