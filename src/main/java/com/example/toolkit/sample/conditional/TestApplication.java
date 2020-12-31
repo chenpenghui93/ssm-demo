c14f957901531c9d36b877b8ba25a20ddffd0c41
chenpenghui <chenpenghui@hisense.com>
2020-12-31 11:18:50 +0800

    @ConditionalOnMissingBean @ConditionalOnBean

--- src/main/java/com/example/toolkit/sample/conditional/TestApplication.java
+++ src/main/java/com/example/toolkit/sample/conditional/TestApplication.java
@@ -19,6 +19,11 @@ public class TestApplication implements CommandLineRunner {
         SpringApplication.run(TestApplication.class, args);
     }
 
+    /**
+     * 根据当前环境或者容器情况动态注入Bean，与@Bean配合使用
+     * @ConditionalOnMissingBean 判断当前需要注入的Spring容器中是否已包含Bean的实现类，如果有则不注入，没有则注入
+     * @ConditionalOnBean 判断当前需要注册的Bean的实现类是否被Spring管理，如果被管理则注入，未被管理则不注入
+     */
     @Override
     public void run(String... args) {
         String eat = animal.eat();
