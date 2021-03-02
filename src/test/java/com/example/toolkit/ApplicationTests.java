8c2f44d9ce423929ab2bf6c714ffcd9e2b1689c2
chenpenghui <chenpenghui@hisense.com>
2021-03-02 17:18:28 +0800

    获取环境变量或系统变量

--- src/test/java/com/example/toolkit/ApplicationTests.java
+++ src/test/java/com/example/toolkit/ApplicationTests.java
@@ -4,10 +4,13 @@ import com.example.toolkit.entity.SysUserEntity;
 import com.example.toolkit.entity.SysUserRoleEntity;
 import com.example.toolkit.service.SysUserRoleService;
 import com.example.toolkit.service.SysUserService;
+import com.example.toolkit.utils.ConfigTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -21,6 +24,28 @@ public class ApplicationTests {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     private SysUserRoleService sysUserRoleService;
+
+    @Autowired
+    private Environment env;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Autowired
+    private ConfigTest configTest;
+
+    /**
+     * 获取环境变量或系统变量三种方式
+     * 1、@Value("${jwt.secret}")
+     * 2、environment.getProperty("jwt.secret")
+     * 3、@ConfigurationProperties
+     */
+    @Test
+    public void t() {
+        System.out.println(secret);
+        System.out.println(env.getProperty("jwt.secret"));
+        System.out.println(configTest);
+    }
 
     /**
      * 注册
